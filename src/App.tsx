c949696c5d1c41cafc25e467ebb445dc09757f8c
Søren Anton Smidt <au754363@uni.au.dk>
2026-02-09 21:31:44 +0100

    Refactored the filtering and sorting logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import type { Country } from "./interface/Country.ts";
 import { CountryCard } from "./CountryCard.tsx";
 import "./App.css"
@@ -7,38 +7,50 @@ export function App() {
 
     const [countries, setCountries] = useState<Country[]>([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [sortBy, setSortBy] = useState<"countryName" | "population">("countryName");
+
 
     useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+        fetch("https://restcountries.com/v3.1/all?fields=name,flags,population")
             .then(res => res.json())
             .then((data: Country[]) => {
-                data.sort((a: Country, b: Country) => a.name.common.localeCompare(b.name.common))
                 setCountries(data);
             })
     }, []);
 
-    const filteredCountries: Country[] = countries.filter((country: Country) => {
-        return country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
-    })
+    const displayedCountries: Country[] = useMemo(() => {
+        const filteredCountries: Country[] = countries.filter((country) =>
+            country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+
+        return [...filteredCountries].sort((a: Country, b: Country) =>{
+            if (sortBy === "population") { return b.population - a.population}
+            return a.name.common.localeCompare(b.name.common)
+        })
+    }, [countries, searchTerm, sortBy])
 
     return (
         <div>
             <header>
                 <h1>World Explorer</h1>
-                <p>Displaying {filteredCountries.length} countries</p>
+                <p>Displaying {displayedCountries.length} countries</p>
             </header>
             <div className={"MainContainer"}>
-                <input
-                    type={"text"}
-                    placeholder="Search... "
-                    onChange={(e) => {
-                        setSearchTerm(e.target.value)
-                        console.log("st: ", searchTerm)
-                    }}
-                    className="SearchInput"
-                />
+                <div className={"SearchTools"}>
+                    <input
+                        type={"text"}
+                        placeholder="Search... "
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value)
+                            console.log("st: ", searchTerm)
+                        }}
+                        className="SearchInput"
+                    />
+                    <button className="SortPopulation" onClick={() => setSortBy("population")}>F</button>
+                </div>
+
                 <div className={"CountryGrid"}>
-                    {filteredCountries.map((country) => (
+                    {displayedCountries.map((country) => (
                         <CountryCard
                             key = {country.name.common}
                             name = {country.name.common}
